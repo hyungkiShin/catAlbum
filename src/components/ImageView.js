3a6dcf8a21b5c16c852fd8258de42511d56a4a2f
신형기 <tkaqkeldk@hanatour.com>
2022-09-04 00:52:29 +0900

    [feat] update

--- src/components/ImageView.js
+++ src/components/ImageView.js
@@ -1,23 +1,42 @@
-const IMAGE_PATH_PREFIX = "https://fe-dev-matching-2021-03-serverlessdeploymentbuck-t3kpj3way537.s3.ap-northeast-2.amazonaws.com/public/images"
+const IMAGE_PATH_PREFIX = 'https://fe-dev-matching-2021-03-serverlessdeploymentbuck-t3kpj3way537.s3.ap-northeast-2.amazonaws.com/public'
 
-export default function ImageView({ $app, initialState }) {
-    this.state = initialState
-    this.$target = document.createElement('div')
-    this.$target.className = "Loading Modal"
-    $app.appendChild(this.$target)
+export default function ImageView({ $app, initialState, modalClose }) {
+  this.state = initialState
+  this.$target = document.createElement('div')
+  this.$target.className = 'Modal ImageView'
 
-    this.setState = (nextState) => {
-        this.state = nextState
-        this.render()
-    }
+  $app.appendChild(this.$target)
 
-    this.render = () => {
-        this.$target.innerHTML = `
-            <div class="content">
-                ${this.state ? `<img src="${IMAGE_PATH_PREFIX}${this.state}" />` : ""}
-            </div>
-        `
-    }
-
+  this.setState = (nextState) => {
+    this.state = nextState
     this.render()
+  }
+
+  this.modalClose = modalClose
+  
+  this.addModalCloseEvent = () => {
+    this.$target.addEventListener('click', (e) => {
+      const $node = e.target.closest('.content')
+  
+      if (!$node) {
+        this.modalClose()
+      }
+    })
+
+    window.addEventListener('keydown', (e) => {
+      if (e.key === "Escape") {
+        this.modalClose()
+      }
+    })
+  }
+
+  console.log('image view ',this.state)
+  this.render = () => {
+    this.$target.innerHTML = `<div class="content">${this.state ? `<img src="${IMAGE_PATH_PREFIX}${this.state}">` : ''}</div>`
+
+    this.$target.style.display = this.state ? 'block' : 'none'
+  }
+
+  this.addModalCloseEvent()
+  this.render()
 }
--- src/components/Loading.js
+++ src/components/Loading.js
@@ -1,23 +1,20 @@
 export default function Loading({ $app, initialState }) {
-    this.state = initialState;
-    this.$target = document.createElement("div");
-    this.$target.className = "Loading Modal";
-
-    $app.append(this.$target);
-
+    this.state = initialState
+    this.$target = document.createElement('div')
+    this.$target.className = 'Loading Modal'
+  
+    $app.appendChild(this.$target)
+  
     this.setState = (nextState) => {
-        this.state = nextState;
-        this.render();
-    };
-
-    console.log(this.state)
-
+      this.state = nextState
+      this.render()
+    }
+  
     this.render = () => {
-        this.$target.innerHTML = `<div class="content">
-                                    <img src="../src/assets/nyan-cat.gif" width="90%"/>
-                                  </div>`;
-        this.$target.style.display = this.state ? "block" : "none";
-    };
-
-    this.render();
-}
+      this.$target.innerHTML = `<div class="content"><img src="../src/assets/nyan-cat.gif" style="width:90%"></div>`
+  
+      this.$target.style.display = this.state ? 'block' : 'none'
+    }
+  
+    this.render()
+  }
